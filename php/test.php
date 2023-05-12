1bf3268236c36020ca8d7219f5fc89f95121b664
BurakAyz <131659405+BurakAyz@users.noreply.github.com>
2023-05-12 16:13:45 +0200

    Add files via upload

---
+++ php/test.php
@@ -0,0 +1,18 @@
+<?php
+//$nmr1 = 12;
+//$nmr2 = 10;
+//$uitkomst = $nmr1 + $nmr2;
+//echo $uitkomst;
+
+
+
+//$mijnNaam = "Carl";
+//$jouwNaam = "Camel";
+//echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+
+$nmr1 = 12;
+$nmr2 = 10;
+$uitkomst = $nmr1 * $nmr2;
+echo $uitkomst;
+?>

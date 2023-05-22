8ca347307fdcf3afdfb16b86db3ff63bab6e2a8e
BurakAyz <131659405+BurakAyz@users.noreply.github.com>
2023-05-22 10:50:46 +0200

    Add files via upload

---
+++ php/PHPweek3.php
@@ -0,0 +1,40 @@
+<?php
+//deel 1
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+
+//echo $myArray[0]. "<br>";
+//echo $myArray[1]. "<br>";
+//echo $myArray[2]. "<br>";
+//echo $myArray[3]. "<br>";
+//echo $myArray[4]. "<br>";
+//echo $myArray[5]. "<br>";
+
+//deel 2
+
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein','step'];
+
+//echo count($myArray);
+
+//deel 3
+
+//regel 2 = bar 
+//regel 5 = world
+//regel 8 = toyota
+//regel 9 = 3 
+//regel 12 = 5
+//regel 19 = five
+
+//deel 4a
+
+//$cijfersPHP = [4.4, 4.6, 5.6, 6.1, 7.6, 7.2];
+
+//$gemiddelde = array_sum($cijfersPHP) / count($cijfersPHP);
+
+//echo $gemiddelde;
+
+//echo(round(5.9166666666667) . "<br>");
+
+//deel 4b
+
+//Ik denk dat het beter is om de regels samen te voegen, omdat het makkelijker is en het is korter
+?> 

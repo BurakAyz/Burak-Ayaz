e3c79a89aea8311e55718444bb7b3356b19d5205
BurakAyz <131659405+BurakAyz@users.noreply.github.com>
2023-06-02 14:23:26 +0200

    Add files via upload

---
+++ php/week4.php
@@ -0,0 +1,20 @@
+<?php
+//for ($i = 0; $i <= 50; $i++) {
+  //  echo $i . " ";
+//}
+
+
+//$namen = array("naam1", "naam2", "naam3", "naam4", "naam5", "naam6", "naam7", "naam8", "naam9","naam10",);
+//foreach ($namen as $namen) {
+//echo $namen . "<br>";
+//}
+
+//$maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+//$i = 1;
+  //  foreach ($maanden as $maanden) { 
+    //    echo " Maand "  . $i . " is " . $maanden . "<br>";
+      //  $i++;
+       // }
+
+?>   
+   
